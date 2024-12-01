2fabceb79c4d7eef7f0f0a04126bbb921e1a0ce9
Nikita <nik3152815@gmail.com>
2024-12-01 21:14:06 +0300

    initial commit

---
+++ src/test/java/itmo/course/coursework/CourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.course.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
