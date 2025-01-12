83c49bdd8b785bafb5a3b93665b6fd19a9df056d
foliageh <mygazztop1@gmail.com>
2025-01-12 21:51:25 +0300

    fix comments

--- src/main/java/itmo/course/coursework/dto/response/CommentDTO.java
+++ src/main/java/itmo/course/coursework/dto/response/CommentDTO.java
@@ -3,10 +3,13 @@ package itmo.course.coursework.dto.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class CommentDTO {
     private Long taskId;
     private UserDTO user;
     private String comment;
+    private LocalDateTime createdAt;
 }
